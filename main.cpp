9b295bc277e62c922be95bf1ae5de5e314cf5404
haozaijun <haozaijun.cc@bytedance.com>
2023-02-27 20:02:04 +0800

    basic plugin register

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <dlfcn.h>
 #include "plugin/registry.h"
 
 int main() {
+    void* lib = dlopen("libcircle_shape.dylib", RTLD_LAZY);
+    if (!lib) {
+        std::cout << "lib not found" << std::endl;
+        return 0;
+    }
+
     auto shape = ShapeFactory::instance().createShape("Circle");
     if (shape)
         std::cout << "shape created" << std::endl;
